090d89114bef87929320d953f702950746c559ab
juztinfied <z_d_143@hotmail.com>
2021-03-03 13:38:48 +0800

    some changes

--- website/views.py
+++ website/views.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session
 
 views = Blueprint('views',__name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        session['imgURL'] = request.form.get('imgURL')
+        session['calcState'] = request.form.get('calcState')
+        print(session.get('imgURL'))
+        return redirect(url_for('views.edit'))
+    else:
+        return render_template('home.html')
+
+@views.route('/edit')
+def edit():
+    print(session.get('calcState'))
+    return "<h1>test</h1>"
